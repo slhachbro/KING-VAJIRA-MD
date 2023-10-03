8ea710778ef4c99a0ebf0cf66a0e8c07f5fd2837
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-10-03 20:53:47 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- commands/reactionpack.js
+++ commands/reactionpack.js
@@ -1,33 +1,33 @@
 /**
 //══════════════════════════════════════════════════════════════════════════════════════════════════════//
-//                                                                                                      //
-//                                ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                                   //
-//                                                                                                      // 
-//                                         Ｖ：１．０．１                                                // 
-//                                                                                                      // 
-//            ███████╗██╗   ██╗██╗  ██╗ █████╗ ██╗██╗         ███╗   ███╗██████╗                        //
-//            ██╔════╝██║   ██║██║  ██║██╔══██╗██║██║         ████╗ ████║██╔══██╗                       //
-//            ███████╗██║   ██║███████║███████║██║██║         ██╔████╔██║██║  ██║                       //
-//            ╚════██║██║   ██║██╔══██║██╔══██║██║██║         ██║╚██╔╝██║██║  ██║                       //
-//            ███████║╚██████╔╝██║  ██║██║  ██║██║███████╗    ██║ ╚═╝ ██║██████╔╝                       //
-//            ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝    ╚═╝     ╚═╝╚═════╝                        //
-//                                                                                                      //
-//                                                                                                      //
-//                                                                                                      //
+                                                                                                      
+                                  ＷＨＡＴＳＡＰＰ ＢＯＴ－ＭＤ ＢＥＴＡ                                   
+                                                                                                      
+                                           Ｖ：１．０．１                                                
+                                                                                                      
+              ██╗   ██╗ █████╗       ██╗██╗██████╗   █████╗         ███╗   ███╗██████╗ 
+              ██║   ██║██╔══██╗      ██║██║██╔══██╗██╔══██╗        ████╗ ████║██╔══██╗
+              ██║   ██║███████║      ██║██║██████╔╝███████║ █████╗██╔████╔██║██║  ██║
+              ╚██╗ ██╔╝██╔══██║██   ██║██║██╔══██╗██╔══██║ ╚════╝██║╚██╔╝██║██║  ██║
+               ╚████╔╝ ██║  ██║╚█████╔╝██║██║  ██║██║   ██║        ██║ ╚═╝ ██║██████╔╝
+                ╚═══╝  ╚═╝  ╚═╝ ╚════╝ ╚═╝╚═╝  ╚═╝╚═╝   ╚═╝        ╚═╝     ╚═╝╚═════╝                         
+                                                                                                      
+                                                                                                     
+                                                                                                      
 //══════════════════════════════════════════════════════════════════════════════════════════════════════//
 
 CURRENTLY RUNNING ON BETA VERSION!!
 *
-   * @project_name : Suhail-Md
-   * @author : Suhail Tech Info
-   * @youtube : https://www.youtube.com/c/@SuhailTechInfo0
-   * @description : Suhail-Md ,A Multi-functional whatsapp user bot.
+   * @project_name : Vajira-Md
+   * @author : Vajira Tech Info
+   * @youtube : https://youtube.com/@gamingewingyt6216
+   * @description : Vajira-Md ,A Multi-functional whatsapp user bot.
    * @version 1.2.2
 *
    * Licensed under the  GPL-3.0 License;
 * 
-   * Created By Suhail Tech Info.
-   * © 2023 Suhail-Md.
+   * ┌┤Created By Vajira Tech Info.
+   * © 2023 Vajira-Md ✭ ⛥.
 * 
    * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
