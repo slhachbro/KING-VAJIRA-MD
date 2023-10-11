7bc829f8d9e42245ab7f0ca069a1a46c051750a6
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-10-11 23:56:55 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- commands/whatsapp.js
+++ commands/whatsapp.js
@@ -52,18 +52,18 @@ const {updateProfilePicture,forwardMessage, getAdmin} = require("../lib")
 
 
 ///============================================================================================
-smd({ pattern: "ptv", desc: "send ptv Message of video", category: "whatsapp",filename: __filename},async(Suhail, msg, text,{cmdName , isCreator}) => {
+smd({ pattern: "ptv", desc: "send ptv Message of video", category: "whatsapp",react: "⚙️",filename: __filename},async(Suhail, msg, text,{cmdName , isCreator}) => {
    if(!msg.quoted) return await msg.send("*Uhh please, reply to a video Message*")
    let mtype = msg.quoted.mtype 
    if(mtype !== "videoMessage") return await msg.send("*Replied Message is not a video, Idiot.*")
    return await forwardMessage(msg.chat, Suhail, msg, cmdName )
 })////------------------------------------------------------------
-smd({pattern: "#", desc: "Save whatsapp status",category: "whatsapp",filename: __filename},async(Suhail, msg, text,{cmdName , isCreator}) => {
+smd({pattern: "#", desc: "Save whatsapp status",category: "whatsapp",react: "⚙️",filename: __filename},async(Suhail, msg, text,{cmdName , isCreator}) => {
    if(!msg.quoted) return await msg.send("*Uhh Please, reply to whatsapp status*")
    let sᴜʜᴀɪʟ_ᴍᴅ_num = await Suhail.bot.decodeJid(msg.user)
    return await forwardMessage(sᴜʜᴀɪʟ_ᴍᴅ_num, Suhail, msg, cmdName )
 })//--------------------------------------------------------------------
-smd({pattern: "save",desc: "Save Message to log number",category: "whatsapp",filename: __filename},async(Suhail, msg, text,{cmdName , isCreator}) => {
+smd({pattern: "save",desc: "Save Message to log number",category: "whatsapp",react: "⚙️",filename: __filename},async(Suhail, msg, text,{cmdName , isCreator}) => {
    if(!isCreator) return await msg.send(tlang().owner)
    if(!msg.quoted) return await msg.send("*Uhh Please, reply to a Message*")
    let sᴜʜᴀɪʟ_ᴍᴅ_num = await Suhail.bot.decodeJid(msg.user)
@@ -76,13 +76,13 @@ smd({ on: "text" }, async (Suhail,msg,text)=> {
    }
 })//==================================================================
 
-smd({pattern: "pp",desc: "Set profile picture",category: "whatsapp",use: '<reply to image>', filename: __filename,},
+smd({pattern: "pp",desc: "Set profile picture",category: "whatsapp",react: "⚙️",use: '<reply to image>', filename: __filename,},
 async(Suhail, msg,text,{cmdName , isCreator}) => {
    if(!isCreator) return await msg.send(tlang().owner);if (!msg.quoted) return await msg.reply("*Reply to an image, dear*");
    if(msg.quoted.mtype !='imageMessage') return await msg.reply("*_Uhh please, Reply to an image._*");    
    const sᴜʜᴀɪʟ_ᴍᴅ_num = await Suhail.bot.decodeJid(msg.user); return await updateProfilePicture(Suhail , sᴜʜᴀɪʟ_ᴍᴅ_num , msg , 'pp' );
  })
-smd({ pattern: "fullpp", desc: "Set full screen profile picture", category: "whatsapp", use: '<reply to image>', filename: __filename,},
+smd({ pattern: "fullpp", desc: "Set full screen profile picture", category: "whatsapp",react: "⚙️", use: '<reply to image>', filename: __filename,},
  async(Suhail, msg,text,{cmdName , isCreator}) => {
    if(!isCreator) return await msg.send(tlang().owner);if (!msg.quoted) return await msg.reply("*Reply to an image, sir*");
    if(msg.quoted.mtype !='imageMessage') return await msg.reply("*_Uhh please, Reply to an image._*");
