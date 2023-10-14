8fe472efd6a100036b299e4a46e2186edd086fbf
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-10-14 07:07:53 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- commands/downloader.js
+++ commands/downloader.js
@@ -401,6 +401,7 @@ async(Suhail, citel, text) => {
     let infoYt = await ytdl.getInfo(urlYt);
     let VidTime = Math.floor(i.timestamp* 60);
     if( VidTime  >= videotime) return await citel.reply(`*_Can't dowanload, video file too big_*`);
+    await citel.send(`_🎶ඔබේ වීඩියෝව භාගත වෙමින් පවති📥 ${info.title}?_`);
     let titleYt = infoYt.videoDetails.title;
     let randomName = `./temp/${vid[1]}.mp4` ;
     const stream = ytdl(urlYt, {   filter: (info) => info.itag == 22 || info.itag == 18, }).pipe(fs.createWriteStream(`./${randomName}`));
