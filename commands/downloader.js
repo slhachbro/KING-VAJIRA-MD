fb401a7dcbc9c78b9e261ac80d1ed6459cf05f9d
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-10-14 05:39:24 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -407,7 +407,7 @@ async(Suhail, citel, text) => {
     let randomName = `./temp/${vid[1]}.mp4` ;
     const stream = ytdl(urlYt, {   filter: (info) => info.itag == 22 || info.itag == 18, }).pipe(fs.createWriteStream(`./${randomName}`));
     await new Promise((resolve, reject) => {stream.on("error", reject);stream.on("finish", resolve);});
-    let buttonMessage = { video: fs.readFileSync(randomName),mimetype: 'video/mp4',caption: "  ✅ඉල්ලු ඔබේ වීඩියෝව✅\nTitle : " + i.title + "\n\n🗺️Description : " + i.timestamp +"\n\n👥Views : "+i.views +"\n\n📥Uploaded : " +i.ago +"\n\n👤Author : "+i.author.name+"\n\nUrl : " + i.url +"\n" + Config.caption ,height: 496, width: 640,}
+    let buttonMessage = { video: fs.readFileSync(randomName),mimetype: 'video/mp4',caption: "  ✅ඉල්ලු ඔබේ වීඩියෝව✅\nTitle : " + i.title + "\n🗺️Description : " + i.timestamp +"\n👥Views : "+i.views +"\n📥Uploaded : " +i.ago +"\n👤Author : "+i.author.name+"\nUrl : " + i.url +"\n" + Config.caption ,height: 496, width: 640,}
     await Suhail.bot.sendMessage(citel.chat, buttonMessage, { quoted: citel })
     try { fs.unlinkSync(randomName) } catch{};
 
@@ -420,7 +420,7 @@ async(Suhail, citel, text) => {
       let meta = { type:"video", quality: info.pref_Quality,}
       let file = await yt.download(vid[1] , meta )
       let thumb = await botpic();
-      file ? await Suhail.bot.sendMessage(citel.chat, { video: {url : file },caption: "  *✅ඉල්ලු ඔබේ වීඩියෝව✅\nTitle : " + i.title + "\n\nUrl : " + i.url +"\n\n🗺️Description : " + i.timestamp +"\n\n👥Views : "+i.views +"\n\n📥Uploaded : " +i.ago +"\n\n👤Author : "+i.author.name+"*\n" + Config.caption ,mimetype: 'video/mp4',jpegThumbnail: log0,height: 496, width: 640 }) :  await citel.send("Video not Found"); 
+      file ? await Suhail.bot.sendMessage(citel.chat, { video: {url : file },caption: "  *✅ඉල්ලු ඔබේ වීඩියෝව✅\nTitle : " + i.title + "\nUrl : " + i.url +"\n🗺️Description : " + i.timestamp +"\n👥Views : "+i.views +"\n📥Uploaded : " +i.ago +"\n👤Author : "+i.author.name+"*\n" + Config.caption ,mimetype: 'video/mp4',jpegThumbnail: log0,height: 496, width: 640 }) :  await citel.send("Video not Found"); 
       try{fs.unlinkSync(`${file}`)}catch{}
     }catch(err) {console.log("ytdl Download video error:", e); console.log("Youtubei Video Download Error :" , err);return await citel.error(`${err} \n\ncmdName : video` )   }
   
