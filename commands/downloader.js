ff5d040486a64134f735cb50a4184e0bd0ba942d
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-10-14 08:54:31 +0530

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -442,9 +442,11 @@ async(Suhail, citel, text) => {
     vid = ytIdRegex.exec(urlYt) || [];
     let info =await yt.getInfo(vid[1]);  
     if( info  && info.duration  >= videotime) return await citel.reply(`*_Can't download, file duration too big_*`);
+    await citel.send(`_🎶ඔබේ ගීතය භාගත කරමින් පවති ${info.title}?_`);
     let file = await yt.download(vid[1],{type : "audio",quality:"best"})	  
     console.log("file:",file)
-    file ? await Suhail.bot.sendMessage(citel.chat, {audio: {url : file } ,mimetype: 'audio/mpeg', }) :  await citel.send("Video not Found"); 
+    file ? await Suhail.bot.sendMessage(citel.chat, {audio: {url : file } ,mimetype: 'audio/mpeg', }) :  await citel.send("Video not Found");
+    await Suhail.bot.sendMessage(citel.chat, buttonMessage, { quoted: citel })
     try{fs.unlinkSync(file)}catch{}
   }catch (e) { console.log(" Play error, "  , e); return citel.error(`${e} \n\ncmdName : play`) }
 })
