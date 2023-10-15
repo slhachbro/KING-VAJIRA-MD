7d824f732813a1fba342910c3804f3049ea6a3c8
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-10-15 08:39:20 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- commands/Auto_Voice.js
+++ commands/Auto_Voice.js
@@ -1,29 +1,21 @@
-const { tlang } = require('../lib')
+if (Config.VOICE_REPLY == 'true') {
+             await Suhail.bot.sendMessage(m.chat, { audio: {url:'https://a.uguu.se/JMpfcKKP.opus'}, mimetype: 'audio/mp4', ptt: true }, { quoted:citel })   
+             }
 
-const Config = require('../config')
+if (Config.VOICE_REPLY == 'true') {
+             await Suhail.bot.sendMessage(m.chat, { audio: {url:'https://a.uguu.se/eNlbkNsU.opus'}, mimetype: 'audio/mp4', ptt: true }, { quoted:citel })   
+             }
 
-const axios = require('axios')
 
-// Put here your url with modified words and urls
+if (Config.VOICE_REPLY == 'true') {
+             await Suhail.bot.sendMessage(m.chat, { audio: {url:'https://a.uguu.se/izRwmLCb.opus'}, mimetype: 'audio/mp4', ptt: true }, { quoted:citel })   
+             }
 
-const url = 'https://gist.github.com/prabathLK/f602911954a959c8730aeb00a588d15d/raw'
 
-/**
-CODED BY MR.CHAMODH
-🎩 *_ᴅᴀʀᴋ Qᴜᴇᴇɴ ᴍᴅ_*🎩
-🎭 *_ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴅᴀʀᴋ Qᴜᴇᴇɴ ᴛᴇᴀᴍ_*🎭
-**/ 
+if (Config.VOICE_REPLY == 'true') {
+             await Suhail.bot.sendMessage(m.chat, { audio: {url:'https://a.uguu.se/oWybuuul.opus'}, mimetype: 'audio/mp4', ptt: true }, { quoted:citel })   
+             }
 
-cmd({ on: "body" }, async (Void,citel,text)=> {
-
- if (Config.autovoice === 'true' && citel.text.startsWith(prefix));
-
-  let { data } = await axios.get(url)
-
-  for (vr in data){
-
- if((new RegExp(`\\b${vr}\\b`,'gi')).test(citel.text)) return Void.sendMessage(citel.chat,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:citel})   
-
-}
-
-})
+if (Config.VOICE_REPLY == 'true') {
+             await Suhail.bot.sendMessage(m.chat, { audio: {url:'https://a.uguu.se/hVumuxFd.mp3'}, mimetype: 'audio/mp4', ptt: true }, { quoted:citel })   
+             }
