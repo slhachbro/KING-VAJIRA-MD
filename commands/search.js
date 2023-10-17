093c4dff707f052e148516c86429a9c7043aa7d5
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-10-17 11:36:15 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- commands/search.js
+++ commands/search.js
@@ -92,6 +92,24 @@ const fetch = require('node-fetch')
                await Suhail.bot.sendMessage(citel.chat, buttonMessaged, { quoted: citel, });
        }
     )
+//------------------------------------------------------------------------------------
+
+smd({
+        pattern: 'news',
+        category: "search",
+        react: "📃",
+        desc: "search hirunews",
+        filename: __filename,
+    },
+    async(Suhail, citel,) ==> {
+        const hirunews = await fetchJson(`https://queen-api.onrender.com/api/news/hiru-news`)
+
+const buttonMessage = {
+    image: {url: hirunews.image },
+    caption: `*${hirunews.title}*\n\n${hirunews.description}\n\n📅 ᴅᴀᴛᴇ ᴀɴᴅ ᴛɪᴍᴇ : *${hirunews.date_time}*\n*⚠️
+       }
+    )
+    
     //------------------------------------------------------------------------------------
 smd({
             pattern: 'ss',
