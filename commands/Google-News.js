e01dcb1bc98839d57a63b5d705a5fef5d040c936
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2023-10-17 11:27:56 +0530

    👑 ＫＩＮＧ - ＶＡＪＩＲＡ 👑

--- commands/Google-News.js
+++ commands/Google-News.js
@@ -1,1 +1,13 @@
+smd({
+        pattern: 'news',
+        category: "search",
+        react: "📃",
+        desc: "search hirunews",
+        filename: __filename,
+    },
+    async(Suhail, citel,) ==> {
+        const hirunews = await fetchJson(`https://queen-api.onrender.com/api/news/hiru-news`)
 
+const buttonMessage = {
+    image: {url: hirunews.image },
+    caption: `*${hirunews.title}*\n\n${hirunews.description}\n\n📅 ᴅᴀᴛᴇ ᴀɴᴅ ᴛɪᴍᴇ : *${hirunews.date_time}*\n*⚠️
